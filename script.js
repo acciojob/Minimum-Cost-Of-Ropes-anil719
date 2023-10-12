72734a04f22fac1d3689aa01b65a342e56d32b6a
Anil Terapalli <117853771+anil719@users.noreply.github.com>
2023-10-12 22:24:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function calculateMinCost() {
   //your code here
-  
-  
+  let cost = 0;
+
+	while (set.length > 1) {
+	  let fmin = Number.MAX_VALUE;
+	  let smin = Number.MAX_VALUE;
+	
+	  for (let i of set) {
+	    if (i < fmin) {
+	      smin = fmin;
+	      fmin = i;
+	    } else if (i < smin) {
+	      smin = i;
+	    }
+	  }
+	
+	  let opCost = fmin + smin;
+	  set.push(opCost);
+	  cost += opCost;
+	
+	  // Remove the first occurrence of fmin and smin from the array
+	  set.splice(set.indexOf(fmin), 1);
+	  set.splice(set.indexOf(smin), 1);
+	}
+	return cost;  
   
 }  
